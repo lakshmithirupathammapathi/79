cd5fb9ba4b846052853865f6f2ce398302f03c1a
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-28 10:49:44 +0530

    Create 79.java

---
+++ 79.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n,m,k;
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	m=sc.nextInt();
+	k=n*m;
+	int sqrt=(int) Math.sqrt(k);
+	if(sqrt*sqrt==k)
+	{
+		System.out.println("yes");
+		}
+		else
+		{
+			
+		 
+		System.out.println("no");
+		
+	}
+}
+}
